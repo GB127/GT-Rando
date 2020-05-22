8ef605322e3c795e49aedde2704d236309a64262
Guylain <guylain.breton.1@ulaval.ca>
2020-05-22 10:22:58 -0400

    New infos!

--- infos.py
+++ infos.py
@@ -1,5 +1,6 @@
 from tools import gethex
 
+nothing = 0
 hookshot = 2
 candle = 4
 greyK = 6
@@ -7,7 +8,7 @@ goldK = 8
 shovel = 10
 bell = 12
 bridge = 14
-listitems = [hookshot, candle, greyK, goldK, shovel, bell, bridge]
+listitems = [hookshot, candle, greyK, goldK, shovel, bell, bridge, nothing]
 
 def itemtable():
     for i in listitems : gethex(i)
@@ -24,10 +25,12 @@ class infos:
 
     def __init__(self):
         self.adresses = {
-            hex(0x143) : "Item 1",
-            hex(0x15D) : "Item 2 related to palette?",
-            hex(0x15C) : "Item 2 related to sprite ID?,",
-            hex(0x142) : "Item 2",
+            hex(0x143) : "Item 1 ID",
+            hex(0x15A) : "Item 1 display",
+            hex(0x15B) : "Item 1 display",
+            hex(0x15D) : "Item 2 display",
+            hex(0x15C) : "Item 2 display",
+            hex(0x142) : "Item 2 ID",
             hex(0x110) : "Xpos p1 (2b)",
             hex(0x113) : "Ypos p1 (2b)",
             hex(0xB6) : "world (1b)",
