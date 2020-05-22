82766fd6a149d340a162d1f9fa5d0b4c3a2e2764
Guylain <guylain.breton.1@ulaval.ca>
2020-05-21 20:43:14 -0400

    Now it checks if it's the correct game as well!

--- gameclass.py
+++ gameclass.py
@@ -6,23 +6,29 @@ class infos:
     """
     def __init__(self):
         self.infos = {
-
                     }
 
 class ROM:
+    header = bytearray([0x47,0x4F,0x4F,0x46,0x20,0x54,0x52,0x4F,0x4F,0x50,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x30,0x00,0x09,0x00,0x01,0x08,0x00,0x2F,0xA5,0xD0,0x5A
+    
+    ,0x20,0x50,0x72,0x6F,0xBC,0xFF,0xBC,0xFF,0xBC,0xFF,0xB4,0xFF,0xBC,0xFF,0xB8,0xFF,0x20,0x4D,0x2E,0x20,0xBC,0xFF,0xBC,0xFF,0xBC,0xFF,0xBC,0xFF,0xB0,0xFF,0xBC,0xFF
+    
+    ])
     def __init__(self,data):
         """
-        This constructor convert the game (that is only in bytes)
-        into a byte array that is a mutable form.
+            First, it checks if it has a header.
+            Then it copies the relevant data.
 
-        data should be in bytes
         """
         if len(data) % 1024 == 512:
-            print("Your game has a header! It's removed")
             self.data = bytearray(data[512:])
-        else:
+        elif len(data) % 1024 == 0:
             self.data = bytearray(data)
+        else:
+            raise BaseException("Your game seems to be corrupted")
+        for n,i in enumerate(self.data[0x7FC0:0x7FFF]):
+            assert i == self.header[n]
     def __getitem__(self,offset):
-        return hex(self.data[offset])
+        return self.data[offset]
     def __setitem__(self,offset, value):
         self.data[offset] = value
