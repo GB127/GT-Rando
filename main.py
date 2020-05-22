5ec2c727f25740d6c21698bc294a94b636da1b09
Guylain <guylain.breton.1@ulaval.ca>
2020-05-21 21:08:17 -0400

    Update main : now it always remove the header!

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from gameclass import ROM
-from checker import gamechecker
 
 with open("Vanilla.smc", "rb") as original:
     originaldata = original.read()
     randogame = ROM(originaldata)
-    gamechecker(randogame)
-    with open("test.smc", "wb") as newgame:
+    with open("vanillanoh.smc", "wb") as newgame:
         newgame.write(randogame.data)
