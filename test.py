d9432d480f83de670e3c37c9260830d60b85d6fb
Guylain <guylain.breton.1@ulaval.ca>
2020-05-23 12:58:02 -0400

    One step toward randomizing items!

---
+++ test.py
@@ -0,0 +1,29 @@
+from tools import gethex
+from gameclass import ROM
+from infos import *
+import random
+
+info = infos()
+
+gethex(0xA9)
+
+
+
+with open("Vanilla.smc", "rb") as original:
+    originaldata = original.read()
+    game = ROM(originaldata)
+
+    # These two lines will mess with all the "get items"
+    # so you will always get the same item everytime!
+    # This is a step toward the randomization of the items.
+    # But this tells me that changing items will be simple.
+    # In summary, the functions only need one value and does all
+    # the dirty works. Note that I've changed some stuffs to make
+    # it load the thing I want.
+
+    # Only need to find the roots how the thing it normally loads is changed.
+    game[0x13067] = 169
+    game[0x13068] = random.randint(0,12)
+
+    with open("vanillanoh.smc", "wb") as newgame:
+        newgame.write(game.data)
