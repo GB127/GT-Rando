cb639e232e6c14b3421de054c3c8de496835797d
Guylain <guylain.breton.1@ulaval.ca>
2020-05-24 14:18:38 -0400

    Some minor changes

--- infos.py
+++ infos.py
@@ -16,7 +16,7 @@ class infos:
         And if I need to get an info during my coding, I'll simply call this class and then ask it to
         Retrieve the things I need.
     """
-    def itemids(self, id=None, adjust=2):
+    def itemids(self, id=None, adjust=0):
         # The adjust is because the items displayed on the levels are
         # always -2. For example, in inventory, the bell is 12. On the
         # level, it will be 10.
@@ -62,17 +62,17 @@ class infos:
 
     def __init__(self):
         self.infos = {
-            hex(0x143) : "item1 ID : infos.itemids(2)",
+            hex(0x143) : "item1 ID : infos.itemids()",
             hex(0x15A) : "item1 display",
             hex(0x15B) : "item1 display",
             hex(0x15D) : "item2 display",
             hex(0x15C) : "item2 display",
-            hex(0x142) : "item2 ID : infos.itemids(2)",
+            hex(0x142) : "item2 ID : infos.itemids()",
             hex(0x110) : "Xpos P1 (2b)",
             hex(0x113) : "Ypos P1 (2b)",
             hex(0xB6) : "world (1b)",
             hex(0xB7) : "lvl (1b)",
-            hex(0x140B) : "Level's Item : infos.itemds(), infos.items140B",
+            hex(0x140B) : "Level's Item : infos.itemds(2), infos.items140B",
                 # Note : the items are always -2. 
                 # For example : the bell will be 10 (or A).
             hex(0x11D) : "P1 Hearts",
@@ -81,7 +81,7 @@ class infos:
             hex(0xBD) : "P # playing : 1 if 1P, 3 if 2P",
             hex(0x140) : "Item P1 check : 2 if has 2 items, else 0",
             hex(0x142) : "Item p1 selected : left = 0, right = 2"
-                    }            
+                    }
     def check(self,adress):
         if isinstance(adress,str):
             return adress + " : " + self.infos[adress]
