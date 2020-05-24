09f30834de965f1b7fad8c1dd34b440e9f919ae8
Guylain <guylain.breton.1@ulaval.ca>
2020-05-23 20:52:19 -0400

    Proof of concept

--- test.py
+++ test.py
@@ -5,23 +5,18 @@ import random
 
 info = infos()
 
-gethex(0xA9)
-
+gethex(0xA9)  # 169
+gethex(0xE8)  # 232
+gethex(0xEA)  # 234
 
 
 with open("Vanilla.smc", "rb") as original:
     originaldata = original.read()
     game = ROM(originaldata)
 
-    # These two lines will mess with all the "get items"
-    # so you will always get the same item everytime!
-    # This is a step toward the randomization of the items.
-    # But this tells me that changing items will be simple.
-    # In summary, the functions only need one value and does all
-    # the dirty works. Note that I've changed some stuffs to make
-    # it load the thing I want.
-    game[0x13067] = 169
-    game[0x13068] = random.choice([0,2,4,6,8,10,12])
+    game[0x65DC] = 0xA9
+    game[0x65DD] = 0xE8
+    game[0x65DE] = 0xEA
 
     with open("vanillanoh.smc", "wb") as newgame:
         newgame.write(game.data)
