d8ed0a6a0dfb6bd660158af89cde1bd6fd9661e8
Guylain <guylain.breton.1@ulaval.ca>
2020-06-16 13:11:10 -0400

    update test.py

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ from gameclass import ROM
 from infos import *
 import random
 from changes import *
+from debug import *
+
+info = infos()
+
 
 with open("Vanilla.smc", "rb") as original:
     originaldata = original.read()
     game = ROM(originaldata)
-    darkrooms_randomizer(game)
-    password_randomizer(game)
-    game[0x6F72] = 0xA
-    game[0x6F77] = 0xB
-
+    level_select(game)
     with open("Vanillanoh.smc", "wb") as newgame:
+        print("Testing case have been created!")
         newgame.write(game.data)
