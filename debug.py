918f9e2453f9b0c84125f3b619ec299bd10baa0c
Guylain <guylain.breton.1@ulaval.ca>
2020-06-15 20:46:48 -0400

    some comments

--- debug.py
+++ debug.py
@@ -1,8 +1,14 @@
-def test_boss_w0(game):
-    # This code changes 0-0 up exit to the boss.
+def test_bosses(game):
+    # This code currently changes only the 0-0 up exit to the boss.
+    # Eventually it will support all 5 bosses for the 5 worlds.
     game[0x1F3ED] = 14
 
-def easy_pass(game):
+def level_select(game):
+    # Put a banana on the box of the world you want to go    
+        # Banana on the 2nd box = World 1 (2nd world of the game)
+        # Banana on the 3rd box = World 2 (3rd world of the game)
+    # Cherry for all the rest of the boxes
+
     Cherry = 0x0
     Banana = 0x1
     RedG = 0x2
