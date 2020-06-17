c4667a818783fd32fe7062e0688d4b2cc26cd6a5
Guylain <guylain.breton.1@ulaval.ca>
2020-06-17 14:23:23 -0400

    remove some comments

--- debug.py
+++ debug.py
@@ -4,19 +4,12 @@ def dark_room(game, world, room):
     game[0x186B6] = room
 
 def test_bosses(game):
-    # This code currently changes only the 0-0 up exit to the boss.
-    # Eventually it will support all 5 bosses for the 5 worlds.
-    # It will also auto-level select because if you want ot test bosses,
-    # You will want ot go there fast for any worlds!
     level_select(game)
     game[0x1F3ED] = 14
     game[0x1F4C3] = 15
     game[0x1F58D] = 25
-
+    # World 3 missing
     game[0x1F877] = 25
-
-def test_room_w0(game, value):
-    game[0x1F3ED] = value
 
 def level_select(game):
     # Put a banana on the box of the world you want to go. Example  
@@ -28,7 +21,6 @@ def level_select(game):
     RedG = 0x2
     BlueG = 0x3
     password = [Cherry, Banana, RedG, BlueG]
-
 
     game.setmulti(0x1C67F, 0x1C692, 0x0)
     game[0x1c680] = 0x1
