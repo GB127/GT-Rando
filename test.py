c5c3c237f538dcd2bd2a996f970a529c048d61d3
Guylain <guylain.breton.1@ulaval.ca>
2020-06-17 19:37:44 -0400

    New info!

--- test.py
+++ test.py
@@ -7,6 +7,13 @@ from debug import *
 info = infos()
 
 
+# patrick gilmore
+"""
+for self.infos:
+    hex(0x1414F) : "Speed of the THE END Credits"
+"""
+
+
 def writetext(game, text,offset):
     for order, letter in enumerate(text):
         game[offset + order] = ord(letter.upper())
@@ -17,6 +24,8 @@ with open("Vanilla.smc", "rb") as original:
     test_bosses(game)
     auto_bosses(game)
 
+
+    game[0x1414F] = 0xFF
     with open("Vanillanoh.smc", "wb") as newgame:
         print("Testing case have been created!")
         newgame.write(game.data)
