eddd1de751827d9a1da5bbe710f94847576189e7
Guylain <guylain.breton.1@ulaval.ca>
2020-06-17 22:42:14 -0400

    More infos and starting some brainstorming

--- test.py
+++ test.py
@@ -13,13 +13,18 @@ for self.infos:
     hex(0x14137) : "Where the THE END should stop"
 """
 
+# Format :
+    # Nombre de "return"/Alignement/Nombre de lettres/couleur/Lettres x Nb/
+    # FF will call the "THE END sprites if it's at "nombre de return"
 
 def writetext(game, text,offset):
     for order, letter in enumerate(text):
         game[offset + order] = ord(letter.upper())
 
-def improve_credits(game):
-    game[0x14137] = 30  # This works
+def credits_writter(game):
+    free_credits_space = 0x5DFF - 0x5FBD4
+    print(free_credits_space)
+    #game[0x14137] = 30  # This works
 
 
 
@@ -30,7 +35,8 @@ with open("Vanilla.smc", "rb") as original:
     test_bosses(game)
     auto_bosses(game)
 
-    game.setmulti(0x14068,0x1406B, 0xEA)
+
+    credits_writter(game)
 
     with open("Vanillanoh.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
