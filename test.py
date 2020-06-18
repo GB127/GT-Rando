d1a60bda9ff5b0af77c41ebddfc37f75e975afca
Guylain <guylain.breton.1@ulaval.ca>
2020-06-17 23:35:41 -0400

    The script works! Now I need to think of a way to write our credits!

--- test.py
+++ test.py
@@ -18,20 +18,23 @@ for self.infos:
     # FF will call the "THE END sprites if it's at "nombre de return"
 
 def writetext(game, text,offset):
-
-
     for order, letter in enumerate(text):
         game[offset + order] = ord(letter.upper())
 
 def credits_writter(game):
     #game[0x14137] = 30  # This works. It's where the THE END will stops"
-    keeper = game[0x5FBC0:0x5FBD3 +1]  # These are the text that scrolls under the THE END
+    vanilla_credits = game[0x5F99E:0x5FbbF +1]
+    stats = game[0x5FBC0:0x5FBD3 +1]  # These are the text that scrolls under the THE END
+    offset = 0x5f99D
+    for value in vanilla_credits:  # Useless, since we rewrite everything back on it but hey I wrote it...
+        offset += 1
+        game[offset] = value
+    for value in stats:
+        offset += 1
+        game[offset] = value
 
-
-    free_credits_space = 0x5FBD4 - 0x5FDFF
-
-
-
+    offset += 1
+    game.setmulti(offset, 0x5FDFF, 0x0)
 
 with open("Vanilla.smc", "rb") as original:
     originaldata = original.read()
