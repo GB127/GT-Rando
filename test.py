9fe5bbd7adcf45dad680ece2d8f6a7f85f322203
Guylain <guylain.breton.1@ulaval.ca>
2020-06-18 14:53:48 -0400

    Some improvements

--- test.py
+++ test.py
@@ -12,18 +12,7 @@ for self.infos:
     hex(0x1414F) : "Speed of the THE END Credits",
     hex(0x14137) : "Where the THE END should stop if password used"
 """
-
-# Format :
-    # Nombre de "return"/Alignement/Nombre de lettres/couleur/Lettres x Nb/
-    # FF will call the "THE END sprites if it's at "nombre de return"
-
-
-# Credits ideas:
-    # Developpers
-    # Eventually, if it gets popular : discord link?
-
-
-# Stats sccreen:
+# Stats screen:
     # Version number
     # Flags
     # Worlds times
@@ -35,30 +24,33 @@ for self.infos:
     # Blue Gems
 
 def add_credits(game, text):
+    # Format :
+    # Nombre de "return"/Alignement/Nombre de lettres/couleur/Lettres x Nb/
+    # FF will call the "THE END sprites if it's at "nombre de return"
+    assert len(text) <= 32, f"Text line too long ({len(text)}). Must be < 32"
+
 
     credits_range = game[0x5F99E: 0x5FFFF +1]
     offset = credits_range.index(0xFF) + 0x5F99E
-    print(hex(offset))
     stats = game[offset: offset +20]
 
-
-    game[offset] = 0x02
+    game[offset] = 0x04  # Nb de returns
     offset += 1
-    assert offset < 0x5FFFF, "Too much text"
-    game[offset] = 0x04
+    assert offset < 0x5FFFF, "Too much text added"
+    game[offset] = 0x00  # Alignement
     offset += 1
-    assert offset < 0x5FFFF, "Too much text"
+    assert offset < 0x5FFFF, "Too much text added"
     game[offset] = len(text)
     offset += 1
-    assert offset < 0x5FFFF, "Too much text"
+    assert offset < 0x5FFFF, "Too much text added"
     game[offset] = 0x30
     for letter in text:
         offset += 1
-        assert offset < 0x5FFFF, "Too much text"
+        assert offset < 0x5FFFF, "Too much text added"
         game[offset] = ord(letter.upper())
     for value in stats:
         offset += 1
-        assert offset < 0x5FFFF, "Too much text"
+        assert offset < 0x5FFFF, "Too much text added"
         game[offset] = value
 
 
@@ -68,22 +60,7 @@ with open("Vanilla.smc", "rb") as original:
     test_bosses(game)
     auto_bosses(game)
 
-    add_credits(game,"Allo Charles, je suis vraiment tanné")
-    add_credits(game,"de regarder les credits")
-    add_credits(game,"pour vérifier si mon code marche")
-    add_credits(game,"et ceci est la preuve que ça marche")
-    add_credits(game,"Enjoy!")
-
-    add_credits(game,"pour vérifier si mon code marche")
-    add_credits(game,"et ceci est la preuve que ça marche")
-    add_credits(game,"Enjoy!")
-
-    add_credits(game,"pour vérifier si mon code marche")
-
-
-
-
-
+    add_credits(game, "Allo charles!")
 
     with open("Vanillanoh.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
