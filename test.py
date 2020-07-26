031dd5188ee91149812b712fda825de9331df3a0
Guylain <guylain.breton.1@ulaval.ca>
2020-07-25 20:55:59 -0400

    Testing case

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 from gameclass import ROM
 from infos import *
 import random
-from changes import *
 from debug import *
 import datetime
 info = infos()
-
-
-
-
 
 with open("Vanilla.smc", "rb") as original:
     originaldata = original.read()
     game = ROM(originaldata)
 
+    start = 0x1F3ED
+    end = 0x1F4FF
+
+    for i in range(start, end+1, 6):
+        print(f"game[{i}] = {hex(game[i])}")
+
+
     with open("Vanillanoh.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
         newgame.write(game.data)
+
+        
