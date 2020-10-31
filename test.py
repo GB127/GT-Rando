c1b384fb354703687b30decc0636a429693973c2
Guylain <guylain.breton.1@ulaval.ca>
2020-10-31 11:55:13 -0400

    Remove the stuffs that was for exits investigating

--- test.py
+++ test.py
@@ -7,13 +7,6 @@ info = infos()
 with open("Vanilla.smc", "rb") as original:
     game = ROM(original.read())
 
-    start = 0x1F3ED
-    end = 0x1F4FF
-
-    for i in range(start, end+1, 6):
-        print(f"game[{hex(i)}] = {hex(game[i])}")
-
-
     with open("Vanillanoh.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
         newgame.write(game.data)
