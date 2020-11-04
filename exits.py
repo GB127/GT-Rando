6bc856b3dc60b8cb62e8efa3adca7faeb1c620fb
Charles <charles342@hotmail.com>
2020-11-03 19:30:16 -0500

    ajout de la classe exits

---
+++ exits.py
@@ -0,0 +1,219 @@
+import random
+from copy import deepcopy
+
+class Exits:
+    def __init__(self, data, world_i):
+        
+        all_nFrames = [16, 16, 26, 30, 26]
+        all_boss_frame = [14, 15, 25, 25, 25]
+        self.nFrames = all_nFrames[world_i]
+        self.boss_frame = all_boss_frame[world_i]
+
+        # getter utilization
+        self.offsets,values,self.source_frames = self.getExitsFromData(data,world_i,self.nFrames)
+        self.nExits = len(values)
+        self.destination_frames = [value[0] for value in values]
+        self.raw_type = [value[3] for value in values]
+        self.destination_Xpos = [value[4] for value in values]
+        self.destination_Ypos = [value[5] for value in values]
+        for i in range(self.nExits):
+                if self.destination_frames[i] == self.boss_frame:
+                    self.preboss_frame = i
+                    break
+
+
+        # other methods
+        self.source_types, self.destination_types = self.getTypes()
+        self.pairs = self.getPairs()
+        
+    def getExitsFromData(self, data, world_i, nFrames):
+        """Fonction allant chercher les offsets ET les valeurs. Je ne sais pas encore si on a besoin 
+            des deux ou pas. Donc j'ai retourné les deux.
+
+            Big thank you to Psychomaniac, reference : https://pastebin.com/y8yYPcfd
+
+            Voici la logique :
+                1- Lecture d'un byte à un endroit précis qui dépend du World. L'info sera un premier offset. C'est la base.
+                    offset 1 = 0x01F303 + World  ou $83:F303 + level_index
+                2- L'endroit de lecture suivant dépend du level et de la lecture précédente. Ça donne un autre offset.
+                    NOTE : ON DOIT LIRE UN "BIG BYTE", DONC ON LIT LE BYTE SUIVANT POUR LE HIGH ENDIAN chose.
+                    offset 2 = 0x1F303 + offset 1 + 2 * level
+                3- La lecture à ce dernier offset donnera l'infos sur la quantité de exits à loader.
+                    0x10000 + offset2
+                4- On load les informations des exits qui sont entreposés dans les cinq offsets par exits.
+                    Le premier débute au offset juste après la lecture à l'étape 3.
+
+            Args:
+                data (bytearray): data du jeu.
+                World (int): World que l'on veut loader.
+
+
+            Return (tuple): exits_offsets, exits_values
+                exits_offsets (liste): Liste de liste qui contient les 6 offsets des exits.
+                exits_values (liste): Liste de liste qui contient les 6 infos des exits.
+                    [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5] ...]
+                    0 : Screen this exit leads to
+                    1 : Base tile index on collision map
+                    2 : Pas spécifié par psychomaniac
+                    3 : Type d'exit, It specifies how the exit's collision tiles are laid down, See below.
+                        If bit 6 is set (0x20) then it is a vertical line, otherwise it is horizontal or 2x2
+                        If bit 6 is not set and bits 1-4 are 0x0F, then the exit is 2x2 (x,y) (x+1,y) (x,y+1) and (x+1,y+1)
+                        For horizontal or vertical lines, bits 1-4 specify the length and bit 5 says if it is 1 or 2 tiles thick.
+                    4 : X position on the destination screen to place Max and/or Goofy
+                    5 : Y position on the destination screen to place Max and/or Goofy
+
+            """
+        # J'ai fait ces deux checks au cas où.
+        assert isinstance(data, bytearray), "Must be a bytearray"
+        assert 0 <= world_i <= 4, "Must be in range 0-4."
+
+        exits_values = []
+        exits_offsets = []
+        exits_frames = []
+
+        # Step 1 : Trouver la base.
+        base = data[0x01F303 + world_i]
+        for frame_i in range(nFrames):
+        # J'ai mis ces lignes en commentaires, car ces lignes forcent l'itération. 
+        # Je pense que l'itération serait beaucoup mieux
+        # Si elle était faite en dehors de la fonction.
+
+        # Comme ça, ça nous permet d'aller chercher spécifiquement les exits à UN endroit souhaité.
+
+                # On doit trouver l'endroit du count.
+
+                # On doit aller chercher le GROS byte. et pour cela, on doit avoir un "adjust" qui est dépendant du frame.
+            adjust = 0x1F303 + base + 2*frame_i
+
+            #Lecture du Gros Byte. On doit lire le byte présent et le byte suivant et les combiner ensemble.
+                # GROS BYTE : 0xHHpp
+            temp1 = data[0x1F303 + base + 2*frame_i]  # Cecu est l'endroit où es tle count, du moins les deux premiers bytes on a les deux premiers chiffres! (pp)
+            temp2 = data[0x1F303 + base + 2*frame_i + 1]  # Les deux high bytes (HH)
+
+            # Donc le fond on doit faire 2 shift left pour ajouter deux zeros. 
+            # Puis additionner.
+                # 0xHH => 0xHH00 => 0xHHpp
+            # Je ne comprends pas pourquoi 16^2 ne fonctionne psa ici.
+
+            temp3 = temp2 * 16 * 16 + temp1  
+
+            # Trouvons enfin l'endroit du count.
+            temp4 = 0x10000 + temp3
+            count = data[temp4]  # This will be the count.
+
+            for i in range(count):
+                exits_offsets.append(list(temp4 + x + 6 * i + 1 for x in range(6)))  # Voici les offsets.
+                # Il devrait être possible d'utiliser Exit() ici.
+                exits_values.append([data[temp4 + x + 6 * i + 1] for x in range(6)])  # Voici les valeurs retrouvées dans chaque offsets.
+                exits_frames.append(frame_i)
+                """
+                    0 : Screen this exit leads to
+                    1 : Base tile index on collision map
+                    2 : Pas spécifié par psychomaniac
+                    3 : Type d'exit, It specifies how the exit's collision tiles are laid down, See below.
+                        If bit 6 is set (0x20) then it is a vertical line, otherwise it is horizontal or 2x2
+                        If bit 6 is not set and bits 1-4 are 0x0F, then the exit is 2x2 (x,y) (x+1,y) (x,y+1) and (x+1,y+1)
+                        For horizontal or vertical lines, bits 1-4 specify the length and bit 5 says if it is 1 or 2 tiles thick.
+                    4 : X position on the destination screen to place Max and/or Goofy
+                    5 : Y position on the destination screen to place Max and/or Goofy
+                """
+        return exits_offsets, exits_values, exits_frames  # On retourne les listes
+    
+    def getPairs(self):
+        pairs = []
+        for i in range(self.nExits):
+            for j in range(self.nExits):
+                if (self.destination_frames[i] == self.source_frames[j])&(self.destination_frames[j] == self.source_frames[i]):
+                    if sorted([i,j]) not in pairs: 
+                        pairs.append(sorted([i,j]))
+        return pairs
+
+    def getTypes(self):
+        source_types = []
+        destination_types = []
+        for i in range(self.nExits):
+            if self.destination_Xpos[i]<25:
+                destination_types.append('W')
+                source_types.append('E')
+            elif self.destination_Xpos[i]>230:
+                destination_types.append('E')
+                source_types.append('W')
+            elif self.destination_Ypos[i]<40:
+                destination_types.append('N')
+                source_types.append('S')
+            elif self.destination_Ypos[i]>190:
+                destination_types.append('S')
+                source_types.append('N')
+            else:
+                destination_types.append('?')
+                source_types.append('?')
+        return source_types, destination_types #source types will not be valid if there had already been a randomization
+
+    def determineRandomizationOrder(self, fix_boss_exit, keep_direction, pair_exits):
+        new_order = list(range(self.nExits))
+        if keep_direction & (not pair_exits):#keep direction
+            for targeted_type in ['N','S','W','E','?']:
+                targeted_i = [i for i, destination_type in enumerate(self.destination_types) if destination_type == targeted_type]
+                if fix_boss_exit: 
+                    if self.preboss_frame in targeted_i: targeted_i.remove(self.preboss_frame)
+                shuffled_i = deepcopy(targeted_i)
+                random.shuffle(shuffled_i)
+                for j,i in enumerate(targeted_i):
+                    new_order[i] = shuffled_i[j]
+
+        elif (not keep_direction) & (pair_exits):#pair exits
+            shuffled_pairs = deepcopy(self.pairs)
+            random.shuffle(shuffled_pairs)
+            for i,pair in enumerate(self.pairs):
+                new_order[pair[0]] = shuffled_pairs[i][0]
+                new_order[shuffled_pairs[i][1]] = pair[1]
+
+        elif keep_direction & pair_exits: #keep direction AND pair exits
+            pairs_to_sort = deepcopy(self.pairs)
+            NS_pairs = []
+            WE_pairs = []
+            stairs_pairs = []
+            for pair in pairs_to_sort:
+                if (self.destination_types[pair[0]] == 'N') or (self.destination_types[pair[0]] == 'S'):
+                    NS_pairs.append(pair)
+                elif (self.destination_types[pair[0]] == 'W') or (self.destination_types[pair[0]] == 'E'):
+                    WE_pairs.append(pair)
+                elif (self.destination_types[pair[0]] == '?'):
+                    stairs_pairs.append(pair)
+            #North and South pairs
+            shuffled_pairs = deepcopy(NS_pairs)
+            random.shuffle(shuffled_pairs)
+            for i,pair in enumerate(NS_pairs):
+                new_order[pair[0]] = shuffled_pairs[i][0]
+                new_order[shuffled_pairs[i][1]] = pair[1]
+            #West and East pairs
+            shuffled_pairs = deepcopy(WE_pairs)
+            random.shuffle(shuffled_pairs)
+            for i,pair in enumerate(WE_pairs):
+                new_order[pair[0]] = shuffled_pairs[i][0]
+                new_order[shuffled_pairs[i][1]] = pair[1]
+            #stairs pairs
+            shuffled_pairs = deepcopy(stairs_pairs)
+            random.shuffle(shuffled_pairs)
+            for i,pair in enumerate(stairs_pairs):
+                new_order[pair[0]] = shuffled_pairs[i][0]
+                new_order[shuffled_pairs[i][1]] = pair[1]
+
+        else: #totally random
+            random.shuffle(new_order)
+            if fix_boss_exit:
+                new_order.remove(self.preboss_frame)
+                new_order.insert(self.preboss_frame,self.preboss_frame)
+
+        return new_order
+
+    def randomize(self, fix_boss_exit, keep_direction, pair_exits): #fix_boss_exit is a bool
+            
+        #determine new order
+        new_order = self.determineRandomizationOrder(fix_boss_exit, keep_direction, pair_exits)
+
+        #assign elements in new order
+        self.destination_frames = [self.destination_frames[i] for i in new_order]
+        self.destination_Xpos = [self.destination_Xpos[i] for i in new_order]
+        self.destination_Ypos = [self.destination_Ypos[i] for i in new_order]
+        self.destination_types = [self.destination_types[i] for i in new_order]
--- world.py
+++ world.py
@@ -1,11 +1,11 @@
 import random
 from copy import deepcopy
-from getters import getter_exits
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-# import cv2  J'ai du commenter cela pour coder mes affaires, car je ne sais pas comment faire pour le régler. J'ai essayé pip install cv2 et ça n'a rien donné.
+import cv2
 from matplotlib.patches import Circle
 import numpy as np
+from exits import *
 
 class World():
     def __init__(self, data, world_i):
@@ -13,131 +13,16 @@ class World():
         assert 0 <= world_i <= 4, "Must be in range 0-4."
 
         all_nFrames = [16, 16, 26, 30, 26]
-        all_bossFrame = [14, 15, 25, 25, 25]
         self.world_i = world_i
         self.data = data
         self.nFrames = all_nFrames[world_i]
-        self.bossFrame = all_bossFrame[world_i]
-        # Si je comprends bien ici, il faudra faire le loop proposé ici pour tout rassembler.
-        self.exits_offsets, self.exits_values, self.source_frames = getter_exits(data, world_i, self.nFrames)
-        self.original_exits_values = deepcopy(self.exits_values)
-        self.determine_current_destinations()
-        self.original_destination_pos = deepcopy(self.destination_pos)
-        self.original_source_pos = deepcopy(self.source_pos) #self.source_pos becomes a useless variable afterwards
-        for i,this_exit_values in enumerate(self.exits_values):
-                if this_exit_values[0] == self.bossFrame:
-                    self.preboss_index = i
-                    break
-        self.determine_current_pairs()
-        self.original_exit_pairs = self.exit_pairs
-        self.nExits = len(self.source_frames)
+        self.exits = Exits(data, world_i)
+        self.original_exits = deepcopy(self.exits)
 
-    def determine_current_destinations(self):
-        self.destination_frames = []
-        self.destination_pos = []
-        self.source_pos = []
-        for this_exit_values in self.exits_values:
-            self.destination_frames.append(this_exit_values[0])
-            if this_exit_values[4]<25:
-                self.destination_pos.append('W')
-                self.source_pos.append('E')
-            elif this_exit_values[4]>230:
-                self.destination_pos.append('E')
-                self.source_pos.append('W')
-            elif this_exit_values[5]<40:
-                self.destination_pos.append('N')
-                self.source_pos.append('S')
-            elif this_exit_values[5]>190:
-                self.destination_pos.append('S')
-                self.source_pos.append('N')
-            else:
-                self.destination_pos.append('?') #stairs
-                self.source_pos.append('?')
+    def randomizeExits(self, fix_boss_exit, keep_direction, pair_exits):
+        self.exits.randomize(fix_boss_exit, keep_direction, pair_exits)
 
-    def determine_current_pairs(self):
-        self.exit_pairs = []
-        for i,source in enumerate(self.source_frames):
-            for j,destination in enumerate(self.destination_frames):
-                if (self.destination_frames[i] == self.source_frames[j])&(self.destination_frames[j] == self.source_frames[i]):
-                    if sorted([i,j]) not in self.exit_pairs: 
-                        self.exit_pairs.append(sorted([i,j]))
-
-    def determine_new_order(self, fix_boss_exit, keep_direction, pair_exits):
-
-        new_order = list(range(self.nExits))
-        if keep_direction & (not pair_exits):#keep direction
-            for ref_destination_pos in ['N','S','W','E','?']:
-                i_to_shuffle_now = [i for i, this_destination_pos in enumerate(self.destination_pos) if this_destination_pos == ref_destination_pos]
-                if fix_boss_exit: 
-                    if self.preboss_index in i_to_shuffle_now: i_to_shuffle_now.remove(self.preboss_index)
-                shuffled_i = deepcopy(i_to_shuffle_now)
-                random.shuffle(shuffled_i)
-                for j,i in enumerate(i_to_shuffle_now):
-                    new_order[i] = shuffled_i[j]
-
-        elif (not keep_direction) & (pair_exits):#pair exits
-            shuffled_pairs = deepcopy(self.exit_pairs)
-            random.shuffle(shuffled_pairs)
-            for i,pair in enumerate(self.exit_pairs):
-                new_order[pair[0]] = shuffled_pairs[i][0]
-                new_order[shuffled_pairs[i][1]] = pair[1]
-
-        elif keep_direction & pair_exits: #keep direction AND pair exits
-            pairs_to_sort = deepcopy(self.exit_pairs)
-            NS_pairs = []
-            WE_pairs = []
-            stairs_pairs = []
-            for pair in pairs_to_sort:
-                if (self.destination_pos[pair[0]] == 'N') or (self.destination_pos[pair[0]] == 'S'):
-                    NS_pairs.append(pair)
-                elif (self.destination_pos[pair[0]] == 'W') or (self.destination_pos[pair[0]] == 'E'):
-                    WE_pairs.append(pair)
-                elif (self.destination_pos[pair[0]] == '?'):
-                    stairs_pairs.append(pair)
-            #North and South pairs
-            shuffled_pairs = deepcopy(NS_pairs)
-            random.shuffle(shuffled_pairs)
-            for i,pair in enumerate(NS_pairs):
-                new_order[pair[0]] = shuffled_pairs[i][0]
-                new_order[shuffled_pairs[i][1]] = pair[1]
-            #West and East pairs
-            shuffled_pairs = deepcopy(WE_pairs)
-            random.shuffle(shuffled_pairs)
-            for i,pair in enumerate(WE_pairs):
-                new_order[pair[0]] = shuffled_pairs[i][0]
-                new_order[shuffled_pairs[i][1]] = pair[1]
-            #stairs pairs
-            shuffled_pairs = deepcopy(stairs_pairs)
-            random.shuffle(shuffled_pairs)
-            for i,pair in enumerate(stairs_pairs):
-                new_order[pair[0]] = shuffled_pairs[i][0]
-                new_order[shuffled_pairs[i][1]] = pair[1]
-
-        else: #totally random
-            random.shuffle(new_order)
-            if fix_boss_exit:
-                new_order.remove(self.preboss_index)
-                new_order.insert(self.preboss_index,self.preboss_index)
-
-        return new_order
-
-    def randomize_exits(self, fix_boss_exit, keep_direction, pair_exits): #fix_boss_exit is a bool
-        
-        #determine new order
-        new_order = self.determine_new_order(fix_boss_exit, keep_direction, pair_exits)
-
-        #assign elements in new order
-        old_exits_values = deepcopy(self.original_exits_values)
-        for i,temp in enumerate(old_exits_values):
-            self.exits_values[i][0] = old_exits_values[new_order[i]][0]
-            self.exits_values[i][4] = old_exits_values[new_order[i]][4]
-            self.exits_values[i][5] = old_exits_values[new_order[i]][5]
-
-        self.determine_current_destinations()
-
-        return self.exits_values
-
-    def show_map(self):
+    def showMap(self):
         
         #map
         filenames = ['map0.png','map1.png','map2.png','map3.png','map4.png']
@@ -165,34 +50,35 @@ class World():
                     horizontalalignment='center', verticalalignment='center')
         
         #exits
-        for i,source in enumerate(self.source_frames):
+        for i,source in enumerate(self.exits.source_frames):
             this_color = list(1-np.random.choice(range(256), size=3)/300)
             #source exit
             this_pos = (origin[0]+frame_positions[source][0]*step_size[0], 
                         origin[1]-frame_positions[source][1]*step_size[1])
-            if self.original_source_pos[i] == 'N':
+            if self.exits.source_types[i] == 'N':
                 source_pos = (this_pos[0],this_pos[1]-step_size[1]*0.4)
-            elif self.original_source_pos[i] == 'S':
+            elif self.exits.source_types[i] == 'S':
                 source_pos = (this_pos[0],this_pos[1]+step_size[1]*0.4)
-            elif self.original_source_pos[i] == 'W':
+            elif self.exits.source_types[i] == 'W':
                 source_pos = (this_pos[0]-step_size[0]*0.4, this_pos[1])
-            elif self.original_source_pos[i] == 'E':
+            elif self.exits.source_types[i] == 'E':
                 source_pos = (this_pos[0]+step_size[0]*0.4, this_pos[1])
-            elif self.original_source_pos[i] == '?':
+            elif self.exits.source_types[i] == '?':
                 source_pos = (this_pos[0]+step_size[0]*0.2, this_pos[1]+step_size[1]*0.2)
+            
             ax.add_patch(Circle(source_pos,5, color=this_color))
             #target exit
-            this_pos = (origin[0]+frame_positions[self.destination_frames[i]][0]*step_size[0], 
-                        origin[1]-frame_positions[self.destination_frames[i]][1]*step_size[1])
-            if self.destination_pos[i] == 'N':
+            this_pos = (origin[0]+frame_positions[self.exits.destination_frames[i]][0]*step_size[0], 
+                        origin[1]-frame_positions[self.exits.destination_frames[i]][1]*step_size[1])
+            if self.exits.destination_types[i] == 'N':
                 target_pos = (this_pos[0],this_pos[1]-step_size[1]*0.4)
-            elif self.destination_pos[i] == 'S':
+            elif self.exits.destination_types[i] == 'S':
                 target_pos = (this_pos[0],this_pos[1]+step_size[1]*0.4)
-            elif self.destination_pos[i] == 'W':
+            elif self.exits.destination_types[i] == 'W':
                 target_pos = (this_pos[0]-step_size[0]*0.4, this_pos[1])
-            elif self.destination_pos[i] == 'E':
+            elif self.exits.destination_types[i] == 'E':
                 target_pos = (this_pos[0]+step_size[0]*0.4, this_pos[1])
-            elif self.destination_pos[i] == '?':
+            elif self.exits.destination_types[i] == '?':
                 target_pos = (this_pos[0]+step_size[0]*0.2, this_pos[1]+step_size[1]*0.2)
             ax.arrow(source_pos[0],source_pos[1],target_pos[0]-source_pos[0], target_pos[1]-source_pos[1], 
                     head_width=15,length_includes_head=True, color=this_color)
@@ -200,42 +86,3 @@ class World():
         plt.show()
         return ''
 
-
-
-class Exit:
-    def __init__(self, list_values):
-        """[summary]
-
-            0 : Screen this exit leads to
-            1 : Base tile index on collision map
-            2 : Pas spécifié par psychomaniac
-            3 : Type d'exit, It specifies how the exit's collision tiles are laid down, See below.
-                If bit 6 is set (0x20) then it is a vertical line, otherwise it is horizontal or 2x2
-                If bit 6 is not set and bits 1-4 are 0x0F, then the exit is 2x2 (x,y) (x+1,y) (x,y+1) and (x+1,y+1)
-                For horizontal or vertical lines, bits 1-4 specify the length and bit 5 says if it is 1 or 2 tiles thick.
-            4 : X position on the destination screen to place Max and/or Goofy
-            5 : Y position on the destination screen to place Max and/or Goofy
-
-
-
-        Args:
-            list_values ([type]): [description]
-        """
-        self.destination = list_values[0]
-        self.data1 = list_values[1]
-        self.data2 = list_values[2]
-        self.type = list_values[3]
-        self.destinationx = list_values[4]
-        self.destinationy = list_values[5]
-
-    def Tuple(self):
-        return self.destination, self.data1, self.data2, self.type, self.destinationx, self.destinationy
-
-    def category(self):
-        """
-            3 : Type d'exit, It specifies how the exit's collision tiles are laid down, See below.
-            If bit 6 is set (0x20) then it is a vertical line, otherwise it is horizontal or 2x2
-            If bit 6 is not set and bits 1-4 are 0x0F, then the exit is 2x2 (x,y) (x+1,y) (x,y+1) and (x+1,y+1)
-            For horizontal or vertical lines, bits 1-4 specify the length and bit 5 says if it is 1 or 2 tiles thick.
-        """
-        pass
