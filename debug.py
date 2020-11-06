e155e75355ce872de9ba74eb2d266e1b760a89ba
Guylain <guylain.breton.1@ulaval.ca>
2020-11-05 19:18:20 -0500

    Empty debug.py

--- debug.py
+++ debug.py
@@ -122,10 +122,6 @@ with open("Vanilla.smc", "rb") as original:
     game = debug(original.read())
     game.world_select()
 
-
-    game.show_map(2)
-
-
     with open("debug.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
         newgame.write(game.data)
