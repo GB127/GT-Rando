c2d0a71426367d37f40c1d7b241aa8e5eb9cf3b4
Guylain <guylain.breton.1@ulaval.ca>
2020-11-06 16:45:11 -0500

    First freespace shenanigans

--- debug.py
+++ debug.py
@@ -7,6 +7,33 @@ from exits import *
 from world import *
 
 class debug(GT):
+
+    def __init__(self,data):
+        self.freespace = [offset for offset in range(0x7374, 0x7FB0)]
+        self.freespace += [offset for offset in range(0xFF33, 0x10000)]
+        self.freespace += [offset for offset in range(0x1401, 0x15E00)]
+        self.freespace += [offset for offset in range(0x1F9C1, 0x1FA42)]
+        self.freespace += [offset for offset in range(0x1FADC, 0x20000)]
+        self.freespace += [offset for offset in range(0x2A796, 0x2C000)]
+        self.freespace += [offset for offset in range(0x47DFE, 0x48000)]
+        self.freespace += [offset for offset in range(0x45055, 0x4F100)]
+        self.freespace += [offset for offset in range(0x4FD48, 0x50000)]
+        self.freespace += [offset for offset in range(0x53F70, 0x54000)]
+        self.freespace += [offset for offset in range(0x55FC0, 0x56000)]
+        self.freespace += [offset for offset in range(0x57E00, 0x58000)]
+        self.freespace += [offset for offset in range(0x5E240, 0x5F000)]
+        self.freespace += [offset for offset in range(0x5FBDC, 0x5FE00)]
+        self.freespace += [offset for offset in range(0xFB5BE, 0xFD400)]
+        self.freespace += [offset for offset in range(0x7FB20, 0x7FE00)]
+        self.freespace += [offset for offset in range(0x7FE50, 0x7FEA0)]
+        self.freespace += [offset for offset in range(0x7FED0, 0x7FF00)]
+        self.freespace += [offset for offset in range(0x7FFB0, 0x7FFFF)]
+
+        self.used = []
+        super().__init__(data)
+
+
+
     def quick_bosses(self):
         self[0xB4AB] = 0x1  # For now, only kill one to clear the boss for world 0.
 
@@ -112,6 +139,12 @@ class debug(GT):
                     toreturn.append((4, no - 88))
         return toreturn
 
+    def __setitem__(self,offset, value):
+        if offset in self.freespace:
+            self.used.append(offset)
+            self.freespace.pop(offset)
+        super().__setitem__(offset,value)
+
 
     def get_first_rooms(self):
         return [(0, self[0x1FFA7]),
@@ -128,7 +161,10 @@ with open("Vanilla.smc", "rb") as original:
     game = debug(original.read())
     game.world_select()
     game.items_randomizer(0)
-    game.show_map(1)
+
+
+    print(len(game.freespace))
+    print(len(game.used))
 
     with open("debug.smc", "wb") as newgame:
         print(f"Testing case have been created! {datetime.datetime.now()}")
