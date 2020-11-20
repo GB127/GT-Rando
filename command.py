e09019ccd5bf815f5c50ee0001fb4563157a0697
Guylain <guylain.breton.1@ulaval.ca>
2020-11-19 21:37:36 -0500

    Fix stuffs

--- command.py
+++ command.py
@@ -7,7 +7,7 @@ def getoptions():
 
     # Dark rooms
         # S for shadow, without shadow there is no dark!
-        # Logic : big D => May be higher than 6, thus capital letter
+        # Logic : big S => May be higher than 6, thus capital letter
     parser.add_argument("-S", "--dark", action="store_true",
     help="Randomize which rooms are dark", dest="Rdark")
     parser.add_argument("-s", "--darkV", action="store_true",
@@ -15,7 +15,7 @@ def getoptions():
 
     # Icy rooms
         # Logic : W for winter, winter = snow and ice, thus icy rooms!
-        # Logic : big I => May be higher than 2, thus capital letter
+        # Logic : big W => May be higher than 2, thus capital letter
     parser.add_argument("-W", "--icy", action="store_true",
     help="Randomize which rooms are icy", dest="Ricy")
     parser.add_argument("-w", "--icyV", action="store_true",
@@ -29,7 +29,7 @@ def getoptions():
     parser.add_argument("-E", "--exits", action="store_true",
     help="Randomize the exits", dest="Rexits")
     parser.add_argument("-B", "--bossE", action="store_false",
-    help="Include the boss exits in the exits pool", dest="Rexits_B")
+    help="Include the boss exits in the exits pools", dest="Rexits_B")
     parser.add_argument("-X", "--xexits", action="store_false",
     help="Do not keep directions", dest="Rexits_X")
     parser.add_argument("-2", "--pairs", action="store_false",
@@ -37,7 +37,6 @@ def getoptions():
 
 
     # Items
-    # This is done!
     parser.add_argument("-I", "--items", action="store_true",
     help="Randomize the items, each world keep their items pool", dest="Ritems")
     parser.add_argument("-Q", "--Ritems", action="store_false",
@@ -47,7 +46,7 @@ def getoptions():
 
     # Seed
     parser.add_argument("--seed", action="store", help="Seed for the randomization",
-                        dest="seed", default=str(random.random())[2:], metavar="", type=int)
+                        dest="seed", default=str(random.random())[2:], metavar="", type=str)
 
     # Password random
     parser.add_argument("--password", action="store_false",
@@ -58,15 +57,15 @@ def getoptions():
     return options
 
 def analyse_options(options):
-    if options.Ritems_random: options.Ritems = True
+    if options.Ritems_random and not options.Ritems:
+        raise BaseException("Completely random items randomzier (Q) must be used with the item randomizer (I)")
+    # B,X,2  Need #E
+    if any([not options.Rexits_B,not options.Rexits_X,not options.Rexits_2]) and (options.Rexits is False):
+        raise BaseException("Boss exits in pools (B), keep direction(X) and pairs(2) must be used with the exits randomizer (E)")
 
-    # Pour ceci, il y aura des changements à faire. 
-    # Mais l'idée est là...
-
-    #if any([options.Rexits_B, options.Rexits_X, options.Rexits_2]) and (options.Rexits is False):
-    #    raise BaseException("Exit randomizer flag must be used if blablabla")
-    #if options.Ritems_random and (options.Ritems is False):
-    #    raise BaseException("Completely random items must be used with The random items flag (I)")
+    # Q needs I
+    if not options.Ritems_random and (options.Ritems is False):
+        raise BaseException("Completely random items must be used with The items randomizer (I)")
 
 
     # Ci-desssous devrait fonctionner.
