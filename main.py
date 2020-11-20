1b802c3012b7751ace9a7686134cdad7a87c2fdb
Guylain <guylain.breton.1@ulaval.ca>
2020-11-19 19:43:07 -0500

    main skeleton!

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from gameclass import ROM, GT
 import argparse
 import random
-
-def getoptions():
-    parser = argparse.ArgumentParser(description="Goof Troop Randomizer, Version alpha", epilog="Written by Niamek & Charles342")
-
-    parser.add_argument("-d", "--dark", action="store_true",
-    help="Randomize which rooms are dark", dest="Rdark")
-
-    parser.add_argument("--password", action="store_false",
-    help="Disable the password randomization", dest="Rpass")
-
-    parser.add_argument("--seed", action="store", help="Seed for the randomization",
-                        dest="seed", default=random.random(), metavar="", type=int)
-    return parser.parse_args()
+from command import getoptions
 
 
 if __name__ == "__main__":
     options = getoptions()
-    random.seed(str(options.seed)[2:] if options.seed < 1 else options.seed)
-        # Note that this isn't used anywhere yet.
-    with open("Vanilla.smc", "rb") as original:
-        originaldata = original.read()
-        randogame = GT(originaldata)
-        randogame.add_credits()
+    random.seed(options.seed)
+    flags = ""  # Pour l'instant. On discutera après pour comment...
+    with open("Vanilla.smc", "rb") as original:  # We will have to change this to not force an exact filename.
+        randogame = GT(original.read())
+        randogame.add_credits() # Will be gone since it the function will be included in the init directly.
+
         if options.Rpass:
             randogame.password_randomizer()
-        if options.Rdark:
-            randogame.darkrooms_randomizer()
-        with open("vanillanoh.smc", "wb") as newgame:
+        if options.Rfirst:
+            pass # Random First frame!
+        if options.Ricy or options.RicyV:
+            # Ricy is if we have more than 2 icy rooms.
+            # RicyV is if we keep 2 icy rooms.
+            pass # Random icy rooms!
+        if options.Rdark or options.RdarkV:
+            # Rdark is if we have more than 6 dark rooms.
+            # RdarkV is if we keep 6 dark rooms.
+            pass  # Random dark rooms!
+        if options.Rfirst:
+            pass  # Random first frame!
+        if options.Exits:
+            pass  # Random exits! using the other options, see command.py
+        if options.Items:
+            pass
+
+        with open(f"{flags}_{options.seed}.smc", "wb") as newgame:
             newgame.write(randogame.data)
