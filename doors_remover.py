2cbf1f1a943efa76e35d58f1c5123b74a6922ec8
Guylain <guylain.breton.1@ulaval.ca>
2020-11-23 11:46:16 -0500

    Clean the code for remover too

--- doors_remover.py
+++ doors_remover.py
@@ -3,18 +3,31 @@ from copy import deepcopy
 def door_remover(data, world, frame, index):  #82C329
     offsets, values = [], []
 
-    this_world_frame_offset = data[0x14461 + world] + frame
-    offset_Y_1 = data[0x14461 + this_world_frame_offset]
+    offset_Y_1 = data[0x14461 + data[0x14461 + world] + frame]
     if offset_Y_1 != 0:
-        offset_base = data[0x144D7 + offset_Y_1]
-        current_offset = (offset_base & 0x00FF) + 0xC4D8
-        vanilla_count = deepcopy(data[0x8000 + current_offset])  # + 1 for testing purpose
+        current_offset = (data[0x144D7 + offset_Y_1] & 0x00FF) + 0xC4D8
+        vanilla_count = deepcopy(data[0x8000 + current_offset])
         data[0x8000 + current_offset] -= 1
-
         if vanilla_count != 0:  # Data collecting
             current_offset += 1
             for _ in range(vanilla_count):
                 tempo, tempo_v = [], []
+                # Tilemap locaton [0-1]
+                tempo.append(0x8000 + current_offset)
+                tempo.append(0x8000 + current_offset + 1)
+                tempo_v.append(data[0x8000 + current_offset])
+                tempo_v.append(data[0x8000 + current_offset + 1])
+
+                # Forme related [2]
+                tempo.append(0x8002 + current_offset)
+                tempo_v.append(data[0x8002 + current_offset])
+                forme = data[0x8002 + current_offset]
+                forme //= 2
+                tempo.append(0x14452 + forme)  # Tilemap format to remove.
+                tempo_v.append(data[0x14452 + forme])
+
+
+                # Format de la porte [3] et ses caractéristiques
                 tempo.append(0x8003 + current_offset)
                 tempo_v.append(data[0x8003 + current_offset])
 
@@ -26,22 +39,6 @@ def door_remover(data, world, frame, index):  #82C329
 
                 bit_to_check = data[0x180B8 + bit_offset]
 
-                tempo.append(0x8000 + current_offset)
-                tempo.append(0x8000 + current_offset + 1)
-
-                tempo_v.append(data[0x8000 + current_offset])
-                tempo_v.append(data[0x8000 + current_offset + 1])
-
-                # This has been done earlier in the code for some reason.
-                tempo.append(0x8002 + current_offset)
-                tempo_v.append(data[0x8002 + current_offset])
-
-
-                something_OxA = data[0x8002 + current_offset]
-                something_OxA //= 2
-
-                tempo.append(0x14452 + something_OxA)  # Tilemap format to remove.
-                tempo_v.append(data[0x14452 + something_OxA])
                 offsets.append(tempo)
                 values.append(tempo_v)
                 current_offset += 4
