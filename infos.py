78a6d61a0ea912d46ea508d0c00468ced696fb8e
Guylain <guylain.breton.1@ulaval.ca>
2020-11-27 19:53:47 -0500

    remove stuffs

--- infos.py
+++ infos.py
@@ -57,12 +57,6 @@ class infos:
             # Item
                 hex(0x140) : "Item P1 check : 2 if has 2 items, else 0",
                 hex(0x142) : "Item P1 selected : left = 0, right = 2",
-                hex(0x143) : "Item1 ID : infos.itemids()",
-                hex(0x142) : "Item2 ID : infos.itemids()",
-                hex(0x15A) : "Item1 display",
-                hex(0x15B) : "Item1 display",
-                hex(0x15D) : "Item2 display",
-                hex(0x15C) : "Item2 display",
 
 
             # P1
@@ -81,51 +75,8 @@ class infos:
             # Credits
                 hex(0x1414F) : "Credits : Speed of the THE END Credits",
                 hex(0x14137) : "Credits : Where the THE END should stop if password used",
-
-
-
-
-            hex(0x10C) : "Player on correct level?"
         }
 
 
     def maps(self):
         print("https://www.vgmaps.com/Atlas/SuperNES/index.htm#GoofTroop")
-
-    def range_dynamic_passwords(self):
-        return [x for x in range(0x230, 0x235)]
-
-    def range_credits(self):
-        print(f' Vanilla credits : {hex(0x5F99E)} - {hex(0x5FBFF)}')
-
-    def format_credits_line(self, verbose=False):
-        print("[Vspac][Hspac][#][Col][...letters...]")
-        if verbose:
-            print("Byte 1   : Vspac : Vertical spacing")
-            print("Byte 2   : Hspac : Horizontal spacing")
-            print("Byte 3   : #     : How many letters to fetch for the line")
-            print("Byte 4   : Col   : Color / Properties (see add_credits for more infos)")
-            print("Bytes 5+ : The following bytes are the letters")
-
-    def range_World_dynamic_Items(self, world=None):
-        """Returns a string showing the range of addresses that stores the items of a said world.
-            If no world is selected, it will print all worlds.
-
-            Args:
-                world ([int], optional): World selected. Defaults to None.
-
-            Raises:
-                InfosError: Make sure the world is only 0-1-2-3-4 or None.
-            """
-        end = {0:0x1164,
-               1:0x1165,
-               2:0x116A,
-               3:0x1163,
-               4:0x1168}
-        if world in range(5):
-            print(f'World {world} range location : {hex(0x1160)} - {hex(end[world])}')
-        elif world is None:
-            for i in end.keys():
-                print(f'World {i} range location : {hex(0x1160)} - {hex(end[i])}')
-        else:
-            raise InfosError("The world can only be a value of [0-1-2-3-4-None]")
