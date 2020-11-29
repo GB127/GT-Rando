8799650058e9c7641bbf1daf64947b4c38271be2
Guylain <guylain.breton.1@ulaval.ca>
2020-11-28 21:03:28 -0500

    further cleaning

--- gameclass.py
+++ gameclass.py
@@ -253,10 +253,7 @@ class GT(ROM):
 
 
     def darkRandomizer(self, count=6):
-        """Randomize which rooms are dark.
-
-            Args:
-                count (int, optional): How many rooms. Defaults to vanilla value (6).
+        """Randomize which rooms are dark up to the count given (Defaults to vanilla value (6)).
             """
         for offset in range(0x1FF35, 0x1FFA7):  # Remove all dark rooms
             self[offset] = self[offset] & 1
@@ -280,11 +277,8 @@ class GT(ROM):
         self[0x1c692] = 0x1
 
     def iceRandomizer(self, count=2):
-        """Randomize which rooms are icy.
-            Args:
-                count (int, optional): How many rooms. Defaults to vanilla value (2).
+        """Randomize which rooms are icy up to the count given (Defaults to vanilla value (2)).
             """
-
         for offset in range(0x1FF35, 0x1FFA7):  # Remove all ice rooms
             self[offset] = self[offset] & 2
         offsets = [offset for offset in range(0x1FF35, 0x1FFA7)]
@@ -299,29 +293,18 @@ class GT(ROM):
         return offsets[world_i] + frame_i + 0x1FF35
 
     def add_credits(self):
-        """
-            This function will add the credits of the contributors of this project
+        """This function will add the credits of the contributors of this project
             in the credits of the game. It defines a function internally, then uses it for each lines.
-        """
+            """
 
         def add_credits_line(self, text,*, center=True, color=0, underlined=False, spacing=0xD):
             """ This function will add a single line to the credits of the game.
-
-            Args:
-                text (str) : The text to be written. Max 32 characters as it's the width of the screen.
-                center (bool, optional): Centered text or not. Defaults to True.
-                color (int, optional): Color, must be 0-15 currently. Defaults to 0 (white).
-                underlined (bool, optional): Will underline the words. Defaults to False.
-                spacing (int, optional): Vertical spacing. Defaults to 0xD.
-            """
+                """
 
             assert len(text) <= 32, f"Text line too long ({len(text)}). Must be < 32"
             assert color <= int("1111", base=2), "0 < Color < 0" # FIXME : I tried to check if color < 0, but couldn't make it work.
 
-            credits_range = self[0x5F99E: 0x5FFFF +1]  
-                # This is the entire range available for writting the credits... Almost. 
-                # I did not make sure it won't scrap the palettes that are stored futher in the region.
-                # Eventually I'll try to fix that. But it seems daunting and tedious... And I'm tired to watch the credits :P
+            credits_range = self[0x5F99E: 0x5FDFF +1]
             offset = credits_range.index(0xFF) + 0x5F99E
                 # 0xFF will call the "THE END sprites if it's at "nombre de return"
                 # In other words, this will fetch the end of the current credits.
@@ -336,29 +319,23 @@ class GT(ROM):
             self[offset] = len(text)  # nombre de lettres
             offset += 1
             self[offset] = color * 4
-                    # byte 0 displayed the text weirdly (jap?)
-                    # byte 1 displayed nothing => If set, always display nothing?
+                    # bit 0 displayed the text weirdly (jap?)
+                    # bit 1 displayed nothing => If set, always display nothing?
+                    # bit 2 - 5 : Color stuffs
                     # byte 6-7 : Mirrors stuffs
-                    # All the others are colors stuffs
 
-                    # By multiplying by 4, we are doing two shift left and 
-                    # then dodge the bits 0 and bits 1 being set.
-                    # Since we can't have bits 6 and 7 set as well, we then cannot
-                    # have an initial color higher than 15 (in binary : 1111).
             for letter in text:  # Writting the string
                 offset += 1
                 self[offset] = ord(letter.upper())
             for value in stats:
                 offset += 1
-                assert offset <= 0x5FFFF, "Too much text added"  # This is the check to make sure we don't pass the allowed range.
-                    # Currently, we can write up to the end of the bank, overwritting the palettes section.
+                assert offset <= 0x5FDFF, "Too much text added"  # This is the check to make sure we don't pass the allowed range.
                 self[offset] = value
             if underlined:
-                # An underline is simply a new line with "¨".
                 string = "¨" * len(text)
                 add_credits_line(self, string ,center=center, color=color, spacing=0x1)
 
-        add_credits_line(self, "Goof Troop randomizer", underlined=True, color=4)
+        add_credits_line(self, "Goof Troop randomizer", underlined=True, color=4, spacing=8)
         add_credits_line(self, "Version alpha", spacing=1)
         add_credits_line(self, f"Seed : {self.seed}", spacing=1)
         add_credits_line(self, "Flags used : alpha", spacing=1)
@@ -372,16 +349,7 @@ class GT(ROM):
         add_credits_line(self, "Zarby89", spacing=2)
 
     def passwordRandomizer(self):
-        """
-            This is the password shuffler.
-
-            All password are shuffled and can be anything.
-            There is also a check to make sure no 2 passwords are identical).
-
-            Summary :
-                randomize each box seperately
-                check if each set of 5 are the same (one world is 5 boxes)
-        """
+        """Password randomizer"""
         password = [0x0, 0x1, 0x2, 0x3]
 
         check = False
