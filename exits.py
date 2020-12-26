eb3fdceaacecc14eb553b8412076e39bb015d9f7
Charles <charles342@hotmail.com>
2020-12-26 01:24:31 -0500

    ménage dans les commentaires

--- exits.py
+++ exits.py
@@ -325,19 +325,19 @@ class Exits:
             elif self.world_i == 3 and source_i == 45 and currently_unlocked[48]==0: #last puzzle of the cave world
                 new_unlocks[source_i] = currently_unlocked[source_i]
             ### one ways temporary fix ###
-            elif self.world_i == 1 and source_i == 15 and currently_unlocked[16]==0: #last puzzle of the cave world
+            elif self.world_i == 1 and source_i == 15 and currently_unlocked[16]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 1 and source_i == 25 and currently_unlocked[26]==0: #last puzzle of the cave world
+            elif self.world_i == 1 and source_i == 25 and currently_unlocked[26]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 2 and source_i == 7 and currently_unlocked[8]==0: #last puzzle of the cave world
+            elif self.world_i == 2 and source_i == 7 and currently_unlocked[8]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 2 and source_i == 27 and currently_unlocked[26]==0: #last puzzle of the cave world
+            elif self.world_i == 2 and source_i == 27 and currently_unlocked[26]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 2 and source_i == 30 and currently_unlocked[29]==0: #last puzzle of the cave world
+            elif self.world_i == 2 and source_i == 30 and currently_unlocked[29]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 2 and source_i == 43 and currently_unlocked[44]==0: #last puzzle of the cave world
+            elif self.world_i == 2 and source_i == 43 and currently_unlocked[44]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
-            elif self.world_i == 2 and source_i == 47 and currently_unlocked[48]==0: #last puzzle of the cave world
+            elif self.world_i == 2 and source_i == 47 and currently_unlocked[48]==0: 
                 new_unlocks[source_i] = currently_unlocked[source_i]
             ### end of temporary fix ###
             elif currently_unlocked[source_i]:
