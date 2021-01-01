0f40d462854cddea4a25ba686f900997a8b6f874
Guylain <guylain.breton.1@ulaval.ca>
2021-01-01 15:10:12 -0500

    start clearing frame!

--- objects.py
+++ objects.py
@@ -24,35 +24,22 @@ class objects:
 
     def transform_byt_co(self, hi_byte, low_byte):
         big_value = hi_byte * 16 * 16 + low_byte
-        # These values will have ot be adjusted
+        # These values will have ot be adjusted as they are wrong, but it's a start.
         y = big_value // 0x80
         x = big_value % 16
         return x, y
 
 
-    def remove_stars(self,world_i, frame_i):
-        # remove all star blocks from world-frame in question.
-        values_else, stars_count = [], 0
-        world_pointer = self[0x014538 + world_i]
-        level_pointer = self[0x14538 + 1 + world_pointer + 2*frame_i] * (16 * 16) + self[0x14538 + world_pointer + 2*frame_i]
-        offset = 0x8000 + level_pointer
-        count = self[offset]
-        for item in range(count):
-            if self[offset +1 + item*3] == 0x1A:
-                stars_count += 1
-            else:
-                values_else += [self[offset +1 + item*3], self[offset +2 + item*3], self[offset +3 + item*3]]
-        new_values = [count -(1 * stars_count)] + values_else
-        old_values = list(self[offset +1 + (3*count):0x15454])
-        self.rewrite(offset, new_values + old_values)
+    def clear_frame(self, world_i, frame_i):
+        world_nframes = [0, 16, 32, 58, 88]
+        level_pointer = self.level_pointers[world_nframes[world_i] + frame_i] - 0xCD41
+        count = self.table[level_pointer]
+        self.table[level_pointer] = 0
+        for _ in range(count * 3):
+            self.table.pop(level_pointer+1)
 
-        # Fixing the next pointers:
-        for offset in range(0x01453D + 2*[0, 16, 32, 58, 88][world_i] + 2*(frame_i+1), 0x014621, 2):
-            try:
-                self[offset] -= stars_count * 0x3
-            except ValueError:
-                self[offset] = self[offset] - stars_count * 0x3 + 256
-                self[offset+1] -= 0x1
+        #TODO: fix pointers 
+
 
 
     def add_stars(self, world_i, frame_i, coordinates):
