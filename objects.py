f7886ab0c99fe3c74b905ccc432c00787999d864
Guylain <guylain.breton.1@ulaval.ca>
2021-01-04 15:00:41 -0500

    Adding work, transfos work!

--- objects.py
+++ objects.py
@@ -35,19 +35,39 @@ class objects:
             objects.append([ID, self.transform_byt_co(self.table[level_pointer+2 + 3*objec], self.table[level_pointer+1 + 3*objec])])
         print(objects)
 
-    def transform_byt_co(self, hi_byte, low_byte):
-        big_value = hi_byte * 16 * 16 + low_byte
-        # These values will have ot be adjusted as they are wrong, but it's a start.
-        y = big_value // 0x80
-        x = big_value % 16
+    def transform_byt_co(self, big_value):
+        assert big_value % 2 == 0, "Cannot be an impair number"
+        y = ( big_value // 0x40) / 2
+        x = (big_value % 0x40) / 2
+        assert x < 31 and 0 < x, "Cannot use this byte"
         return x, y
 
     def transform_co_byt(self, x, y):
-        # These values will have ot be adjusted as they are wrong, but it's a start.
-        transfo_y = y * 0x80
-        big_value = transfo_y + x
-        return big_value
+        assert x <= 30 and 0 < x, f"X cannot be {x}."
+        #TODO : Y
+        assert x % 0.5 == 0 and y % 0.5 ==0 , "X or Y cannot be this value, can only be a multiple of 0.5"
+        transfo_y = y * 2 * 0x40
+        big_value = transfo_y + x * 2
+        return int(big_value)
 
+
+
+    def add_objects(self, world_i, frame_i, object_ids, objects_co):
+        world_nframes = [0, 16, 32, 58, 88]
+        level_pointer = self.level_pointers[world_nframes[world_i] + frame_i] - 0xCD41
+        self.table[level_pointer] += len(object_ids)
+        for no, objec in enumerate(object_ids):
+            tempo_big = self.transform_co_byt(objects_co[no][0], objects_co[no][1])
+            tempo_hi = (tempo_big & 0xFF00) // 16 // 16
+            tempo_low = (tempo_big & 0xFF)
+
+            self.table.insert(level_pointer + 1, tempo_hi)
+            self.table.insert(level_pointer + 1, tempo_low)
+            self.table.insert(level_pointer + 1, objec)
+
+
+        #Pointers fix
+        self.level_pointers[level_pointer+1:] = [x + 3*len(object_ids) for x in self.level_pointers[level_pointer+1:]]
 
 
 
@@ -62,29 +82,3 @@ class objects:
 
         #Pointers fix
         self.level_pointers[level_pointer+1:] = [x - 3*count for x in self.level_pointers[level_pointer+1:]]
-
-    def add_stars(self, world_i, frame_i, coordinates):
-        values_stars = []
-        for coordinate in coordinates:
-            values_stars += [0x1A, coordinate&0xFF, (coordinate&0xFF00) // 16 // 16]
-        values_else= []
-        world_pointer = self[0x014538 + world_i]
-        level_pointer = self[0x14538 + 1 + world_pointer + 2*frame_i] * (16 * 16) + self[0x14538 + world_pointer + 2*frame_i]
-        offset = 0x8000 + level_pointer
-        count = self[offset]
-        for item in range(count):
-            if self[offset +1 + item*3] == 0x1A:
-                values_stars += [self[offset +1 + item*3], self[offset +2 + item*3], self[offset +3 + item*3]]
-            else:
-                values_else += [self[offset +1 + item*3], self[offset +2 + item*3], self[offset +3 + item*3]]
-        new_values = [count +len(coordinates)] + values_else + values_stars
-        old_values = list(self[offset +1 + (3*count):0x15452])
-        self.rewrite(offset, new_values + old_values)
-
-        # Fixing the next pointers:
-        for offset in range(0x01453D + 2*[0, 16, 32, 58, 88][world_i] + 2*(frame_i+1), 0x014621, 2):
-            try:
-                self[offset] += 0x3 * len(coordinates)
-            except ValueError:
-                self[offset] += 0x3 * len(coordinates) - 256
-                self[offset+1] += 0x1
