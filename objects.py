f11f622bd80febea367da092c8e0e1e1575d23cb
Guylain <guylain.breton.1@ulaval.ca>
2021-01-05 19:15:01 -0500

    Finally, objects has a string and a working transformer or something

--- objects.py
+++ objects.py
@@ -13,6 +13,31 @@ class objects:
         self.table = list(self.data[0x14D41:0x15452])
 
 
+
+    def print_world(self, world_i, frame_i):
+        boundary_top = "_" * 34
+        boundary_bottom = "¯" * 34
+        string_list = []
+        for _ in range(28):
+            string_list.append("|" + " " * 32 + "|")
+
+        for one in self.get_world_frame(world_i, frame_i):
+            x = int(2*one[1][0])
+            y = int(2*one[1][1])
+            print(x, y)
+            string_list[y] = string_list[y][:x+1] + "XX" + string_list[y][x+3:]
+            string_list[y+1] = string_list[y+1][:x+1] + "XX" + string_list[y+1][x+3:]
+
+
+        string = "\n".join(string_list)
+
+
+        print(boundary_top)
+        print(string)
+        print(boundary_bottom)
+
+
+
     def save(self):
         # Works, nothing to change here except optimization if you have any...
         tempo_newdata = []
@@ -26,29 +51,40 @@ class objects:
             self.data[0x14D41 + no] = new_data
 
     def get_world_frame(self, world_i, frame_i):
+        # Not sure we'll need this, we'll see.
         world_nframes = [0, 16, 32, 58, 88]
         level_pointer = self.level_pointers[world_nframes[world_i] + frame_i] - 0xCD41
         count = self.table[level_pointer]
         objects = []
         for objec in range(count):
             ID = (self.table[level_pointer+1 + 3*objec])
-            objects.append([ID, self.transform_byt_co(self.table[level_pointer+2 + 3*objec], self.table[level_pointer+1 + 3*objec])])
-        print(objects)
+            objects.append([ID, self.transform_byt_co(
+                                    self.transform_small_big(
+                                        self.table[level_pointer+2 + 3*objec], 
+                                        self.table[level_pointer+3 + 3*objec]))
+                            ])
+
+        return objects
+
 
     def transform_byt_co(self, big_value):
-        assert big_value % 2 == 0, "Cannot be an impair number"
+        assert big_value <= 0x6bc and big_value >=0, "Byte value must be 0 to 0x6BC"
+        assert big_value % 2 == 0, "Byte value must be pair, else it will appear gliched."
+
         y = ( big_value // 0x40) / 2
         x = (big_value % 0x40) / 2
-        assert x < 31 and 0 < x, "Cannot use this byte"
-        return x, y
+        return x/2, y
 
     def transform_co_byt(self, x, y):
-        assert x <= 30 and 0 < x, f"X cannot be {x}."
-        #TODO : Y
-        assert x % 0.5 == 0 and y % 0.5 ==0 , "X or Y cannot be this value, can only be a multiple of 0.5"
+        assert x <= 15 and x >= 0, "X must be in 0 and 15"
+        assert y <= 13 and y >= 0, "Y must b in 0 and 13"
+        assert x % 0.5 == 0 and y % 0.5 == 0, "X or Y must be a multiple of 0.5"
         transfo_y = y * 2 * 0x40
-        big_value = transfo_y + x * 2
+        big_value = transfo_y + x * 2 *2
         return int(big_value)
+
+    def transform_small_big(self, low, hi):
+        return hi * 16 * 16 + low
 
 
 
