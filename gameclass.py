46145f1c4db051f761acc7b7430426cfc52bca9c
Guylain <guylain.breton.1@ulaval.ca>
2021-08-12 21:49:12 -0400

    Fix the room to index, should be good now

--- gameclass.py
+++ gameclass.py
@@ -8,15 +8,13 @@ def get_world_indexs(world):
 
 
 def room_to_index(tup=None, id=None):
-        all_nFrames = [16, 16, 26, 30, 26]
-
-        id_per_world =   [0, 15, 31, 57, 88]
+        id_per_world =   [0, 16, 32, 58, 88]
         if tup:
             return id_per_world[tup[0]] + tup[1]
         if id:
             for world, borne in enumerate(id_per_world[-1::-1]):
-                if id > borne:
-                    return (4 - world, id - borne - 1)
+                if id >= borne:
+                    return (4 - world, id - borne)
         if id == 0:
             return (0,0)
 
