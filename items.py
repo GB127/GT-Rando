732ebc90d97a684b76875dba7981f8a50c55e07b
Guylain <guylain.breton.1@ulaval.ca>
2022-11-25 08:27:25 -0500

    Add some items logic

--- items.py
+++ items.py
@@ -21,7 +21,7 @@ class Items:
             objet_type = self.data.screens[screen_id].class_2_sprites[objet_id].type
             if objet_type in range(0x8, 0xF) or objet_type in range(0x40, 0x47, 2):
                 items += [objet_type]
-        if item_id:
+        if isinstance(item_id, int):
             return items[item_id]
         else:
             return items
@@ -108,11 +108,13 @@ class Items:
                 break
 
     def nodes(self, sorties):
-        items_names = {0x8 : "Hookshot", 0xA : "Grey Key",0xB : "Gold Key", 0xE : "Bridge"}
+        items_names = {0x8 : "Hookshot", 0xA : "Gray Key",0xB : "Gold Key", 0xE : "Bridge"}
         items_screens = {7:("7 (W)",),  # W0 : plank 
                         9:("9 (S)",),  # W0 : Shovel
+                        23:("7 (C)", "7 (C)", " 7 (C)"),  # W1 : Hookshot + plank
                         29:("13 (N)",),  # W1 : Item north of double hookshot.
                         40:("8 (S)", "8 (S)", "8 (S)", "8 (S)", "8 (↗)", "8 (↗)"),  # W2 : Corridor with fruits and gem
+                        42:("10 (N)", "10 (N)", "10 (N)"),  # W2 : Plank Room. Fix all to north since it's easier for logic from north.
                         49:("17 (N)",),  # W3 : Screen with gray key in middle, item on the right behind hookshot
                         53:("21 (W)", "21 (W)", "21 (↗)")  # W3 : movable platform and hookshot
                         }#, 53, 55, 62, 65, 69, 77, 78, 90, 94, 95, 96, 97, 99, 100, 101, 102, 103, 104, 105, 106, 108, 109, 111
@@ -128,6 +130,7 @@ class Items:
                 for id, item in enumerate(self[screen]):
                     if item in items_names:
                         g.add_edge(stringed, (B7, items_names[item], id))
+
         return g
 
 
