a80f9e8786663e6ae80793ceacf5242c3bffa5a2
Guylain <guylain.breton.1@ulaval.ca>
2022-11-25 08:30:52 -0500

    First approach of recursion with networkx

--- doors.py
+++ doors.py
@@ -1,227 +1,52 @@
+from generic import room_to_index
 from copy import deepcopy
+import networkx as net # version 2.5
 
 class Doors():
-    def __init__(self, data, world_i, exits):
-        all_nFrames = [16, 16, 26, 30, 26]
-        self.nFrames = all_nFrames[world_i]
+    def __init__(self, data, world_i,screens):
+        # For now, it's fixed. So no calculs or calling to do.
+        self.data = data
+        self.world_i = world_i
+        self.screens = screens
 
-        self.positions_offsets = []
-        self.positions = []
-        self.shape_types = []
-        self.lock_bit_i = []
-        self.frames = []
-        self.boss_door = []
-        for frame_i in range(self.nFrames):
-            results = self.getKeyDoorsFromData(data, world_i, frame_i)
-            if results:
-                for elem in results:
-                    self.positions_offsets.append(elem[0])
+    def nodes(self, exits, items):
+        g = net.DiGraph()
+        items_names = {0x8 : "Hookshot", 0xA : "Gray Key",0xB : "Gold Key", 0xE : "Bridge"}
 
-                    self.positions.append(data[elem[0] +1]*16*16 + data[elem[0]])
-
-                    self.shape_types.append(elem[1])
-                    self.lock_bit_i.append(elem[2])
-                    self.boss_door.append(elem[3])
-                    self.frames.append(frame_i)
-        self.nDoors = len(self.frames)
-
-        self.types = self.getDoorTypes()
-        self.locked_exits = self.getLockedExits(exits)
-
-    def getDoorTypes(self):
-        types = []
-
-        N_list = [14]
-        S_list = [846]
-        W_list = [256, 448]
-        E_list = []
-        center_list = [526, 654]
-        
-        for door_i in range(self.nDoors):
-            if self.positions[door_i] in N_list:
-                types.append('N')
-            elif self.positions[door_i] in S_list:
-                types.append('S')
-            elif self.positions[door_i] in W_list:
-                types.append('W')
-            elif self.positions[door_i] in E_list:
-                types.append('E')
-            elif self.positions[door_i] in center_list:
-                types.append('?')
-            else:
-                raise DoorTypeError('Could not assign a type to this door')
-        
-        return types
-
-    def getLockedExits(self, exits):
-        locked_exits = []
-        for door_i in range(self.nDoors):
-            for exit_i in range(exits.nExits):
-                if (self.frames[door_i] == exits.source_frames[exit_i]) and (self.types[door_i] == exits.source_types[exit_i]):
-                    locked_exits.append(exit_i)
-                    break
-
-        return locked_exits
+        all_locks = {
+                    5: {"Gray Key": [("5 (N)","8 (S)"), ("8 (S)", "5 (N)")]},
+                    8: {"Gray Key": [("8 (S)","5 (N)"), ("5 (N)","8 (S)") ]},
+                    7: {"Bridge" :[("7 (N)", "7 (S)"), ("7 (S)", "7 (N)")]},
+                    
+                    23: {"Hookshot" : [("7 (S)", "7 (C)"), ("7 (C)", "7 (S)")],
+                            "Bridge" : [("7 (C)", (7, items_names[items[23,0]], 0)), ("7 (C)", (7, items_names[items[23,1]], 1))]},
+                    24: {   "Hookshot": [("8 (N)", (8, items_names[items[24,0]] , 0)), ("8 (N)", (8, items_names[items[24,1]] , 1))],
+                            "Gray Key": [("8 (N)", exits.find("8 (N)"))]}, # W1 door just before the worst puzzle
+                    26: {"Gray Key": [("10 (N)", "12 (S)")]}, # W1 door on the water screen.
+                    28: {"Gray Key": [("12 (S)", "10 (N)")]}, # W1 door on screen north of the water screen.
 
 
+                    35: {"Gray Key": [("3 (N)",  exits.find("3 (N)")),]},  # W2 : Screen with holes
+                    42: {"Bridge": [("10 (E)", "10 (C)"),("10 (C)", "10 (E)"), ("10 (N)",(10, items_names[items[42,0]] , 0))]},
+                    44: {"Gray Key": [("12 (S)", exits.find("12 (S)")),]}, # W2 : Screeen with some pyramids and enemies
+                    45: {"Gray Key": [("13 (W)",  exits.find("13 (W)")),]},  # W2 : Boulder screen
+                    49: {   "Hookshot": [("17 (N)", (17, items_names[items[49,0]] , 0))],
+                            "Gray Key": [("17 (N)","17 (↗)"),]},  # W2 : Center Gray lock door
+                    51: {"Gray Key": [("19 (S)", exits.find("19 (S)")),]},  # W2 : Screen just before the center gray
+                    53: {   "Hookshot": [("21 (W)", "21 (↗)"), ("21 (↗)", "21 (W)")],
+                            "Gray Key": [("21 (W)",  exits.find("21 (W)"),),]},  # W2 : moving platform!
 
-    def getKeyDoorsFromData(self, data, world_i, frame_i):  #82C329
-        """Get all doors of said world-frame.
+                    80: {"Hookshot": [("22 (N)", "24 (S)"), ("24 (S)", "22 (N)")]},  # Waterfall
 
-            Data structure:
-                (0, 1) : Où sur l'écran
-                2 : Forme de la porte (Pour pouvoir bien l'éffacer)
-                3 : format de la porte
-                    Bit 0-2 : offset for getting the correct bit
-                    Bit 3-6 : 0x1144 + X depending on the bit sets.
-                        Sera tjs 0x1144 ou 0x1145 dans vanilla.
-                    Bit 7 : Détermine si la porte est une porte de boss
-                        ou une porte normale.
+                    100:{   "Hookshot" : [("12 (N)", "12 (S)"), ("12 (S)", "12 (N)")],
+                            "Gray Key": [("12 (N)", exits.find("12 (N)")),]}  # W4 : Firebreather screen
+                                    }
+        self.world_locks = {}
+        for screen in all_locks:
+            if screen in self.screens:
+                self.world_locks[screen] = all_locks[screen]
+                for list_nodes in all_locks[screen].values():
+                    for node1, node2 in list_nodes:
+                        g.add_edge(node1, node2)
+        return g
 
-            Returns:
-                (0, 1) : Où sur l'écran
-                2 : Forme de la porte (Pour pouvoir bien l'éffacer)
-                3 : Indice checké par la porte
-                4 : 1 if boss door
-            """
-        result = []
-        offset_Y_1 = data[0x14461 + data[0x14461 + world_i] + frame_i]
-        if offset_Y_1 != 0:
-            current_offset = (data[0x144D7 + offset_Y_1] & 0x00FF) + 0xC4D8
-            count = data[0x8000 + current_offset]
-            if count != 0:
-                current_offset += 1
-                for _ in range(count):
-                    base_door = 0x8000 + current_offset
-                    map_tile_offset = base_door  # [0] et [1], c'est une grosse lecture!
-                        # C'est avec cette info ci-dessus que l'on va pouvoir déterminer si C'est une porte
-                        # N,S,E ou W.
-                    shape = data[base_door + 2]
-                    door_data = data[base_door + 3]
-
-                    # Manipulations à faire pour récupérer la bonne information pour la logique.
-                    bit_offset = door_data & 0x07
-                    which_door = (int((door_data & 0x7F) / 2 / 2 / 2) * 8) + bit_offset
-
-                    boss = (door_data & 0x80) // 0x80
-                    result.append([map_tile_offset, shape, which_door, boss])
-
-                    current_offset += 4
-        return result
-
-
-def door_adder(data, world_i, frame_i, tile_high, tile_low, forme, which_door):  #82C329
-    """Ajoute une porte.
-
-        IMPORTANT : Toujours retirer une porte présente avant d'utiliser cette fonction.
-        Le code est entièrement fonctionnel. Mais l'emplacement actuel des données de portes
-        ne permet pas d'ajouter des portes et je devrai les déplacer.
-
-        Aussi, l'ajout d'une porte est purement mécanique : le visuel n'est pas changé et donc
-        pour tester cela, il faut vraiment localisre où la mécanique est ajoutée et utiliser une clé.
-
-        Args:
-            data ([type]): data of the game.
-            world_i (int): World you want to add a door.
-            frame_i ([type]): frame you want to add a door.
-            tile_high ([type]): [description]
-            tile_low ([type]): [description]
-            forme (0, 4 ou 2): Forme de la porte.
-                Pourrait disparaître si on comprend le tile_high/low.
-                0 = N
-                4 = S
-                2 = EW
-            which_door (0 - 32): Which bit to check if locked or not.
-                In vanilla, it will never fo over 15 (16 bits available, but not all used).
-        """
-
-    offset_Y_1 = data[0x14461 + data[0x14461 + world_i] + frame_i]
-    if offset_Y_1 != 0:
-        current_offset = (data[0x144D7 + offset_Y_1] & 0x00FF) + 0xC4D8
-        vanilla_count = deepcopy(data[0x8000 + current_offset])
-        data[0x8000 + current_offset] += 1
-        current_offset += 1
-        if vanilla_count != 0:
-            for _ in range(vanilla_count): current_offset += 4
-        base_door = 0x8000 + current_offset
-
-        data[base_door] = tile_low
-        data[base_door + 1] = tile_high
-        data[base_door + 2] = forme
-
-        # manips pour convertir which_door en données comprenable pour le jeu.
-        bits_3_6 = (which_door // 8)  * 2 * 2 * 2
-        bits_0_2 = which_door % 8
-        data[base_door + 3] = bits_0_2 + bits_3_6
-        # Je crois que bit # 7 c'est pour la clé du boss. À vérifier.
-
-
-def door_remover(data, world, frame, index):  #82C329
-    """Retire une porte. Ça retire le mécanisme de déverouillage d'une porte.
-    Cependant, le visuel et le physique demeure : les portes resteront là même
-    si retirées à l'aide de la présente fonction.
-
-    Args:
-        data (bytearray): game data
-        world ([type]): World
-        frame ([type]): Frame
-        index ([type]): Which door to remove
-            In case a frame has more than one key locked door.
-            It never happen in vanilla. But it works.
-    """
-
-    offsets, values = [], []
-
-    offset_Y_1 = data[0x14461 + data[0x14461 + world] + frame]
-    if offset_Y_1 != 0:
-        current_offset = (data[0x144D7 + offset_Y_1] & 0x00FF) + 0xC4D8
-        vanilla_count = deepcopy(data[0x8000 + current_offset])
-        data[0x8000 + current_offset] -= 1
-        if vanilla_count != 0:  # Data collecting
-            current_offset += 1
-            for _ in range(vanilla_count):
-                tempo, tempo_v = [], []
-                # Tilemap locaton [0-1]
-                tempo.append(0x8000 + current_offset)
-                tempo.append(0x8000 + current_offset + 1)
-                tempo_v.append(data[0x8000 + current_offset])
-                tempo_v.append(data[0x8000 + current_offset + 1])
-
-                # Forme related [2]
-                tempo.append(0x8002 + current_offset)
-                tempo_v.append(data[0x8002 + current_offset])
-                forme = data[0x8002 + current_offset]
-                forme //= 2
-                tempo.append(0x14452 + forme)  # Tilemap format to remove.
-                tempo_v.append(data[0x14452 + forme])
-
-
-                # Format de la porte [3] et ses caractéristiques
-                tempo.append(0x8003 + current_offset)
-                tempo_v.append(data[0x8003 + current_offset])
-
-                door_data = data[0x8003 + current_offset]
-                bit_offset = door_data & 0x07
-
-                tempo.append(0x180B8 + bit_offset)
-                tempo_v.append(data[0x180B8 + bit_offset])
-
-                bit_to_check = data[0x180B8 + bit_offset]
-
-                offsets.append(tempo)
-                values.append(tempo_v)
-                current_offset += 4
-
-    # Décalage des valeurs au cas où un frame aurait plus qu'une porte barrée.
-        # N'arrivera jamais en vanilla. Mais possiblement au rando et donc on couvre
-        # cette possibilité avec ceci!
-
-    for kit_offset in offsets[index:]:
-        tempo = []
-        current_index = offsets.index(kit_offset)
-        try:
-            for no, offset in enumerate(kit_offset):
-                data[offset] = values[current_index+1][no]
-                tempo.append(data[offset])
-        except IndexError:
-            pass
--- world.py
+++ world.py
@@ -1,6 +1,9 @@
-from generic import world_indexes
+from generic import world_indexes, LogicError
 from items import Items
+import matplotlib.pyplot as plt
 from exits import Exits
+from doors import Doors
+from copy import deepcopy
 import networkx as net # version 2.5
 
 class World():
@@ -11,16 +14,119 @@ class World():
 
         self.Exits = Exits(self.data,self.world_i,self.screens)
         self.Items = Items(self.data, self.world_i, self.screens)
-
+        self.Doors = Doors(self.data, self.world_i, self.screens)
 
     def __call__(self):
         self.Exits(randomize=True, keep_direction=True, pair_exits=True)
         self.Items(randomize_items=True)
-        self.nodes()
+
+
+    def __bool__(self):
+        # World 0 : Vanilla Works
+        # World 1 : Vanilla Works
+        # World 2 :
+        # World 3 : Vanilla will work once we take care of the internal puzzles that makes some paths unidirect.
+        # World 4 :
+        def get_items(graphik, spawn) -> list:
+            """Get all accessible items from specified spawn."""
+            items = []
+            for access in net.shortest_path(graphik, spawn).keys():
+                if isinstance(access, tuple):
+                    items.append(access)
+            return items
+
+        def get_locks(graphik,locks, spawn) -> list:
+            """Get all accessible locks from a specified spawn."""
+            accessible_locks = set()
+            for screen_lock in locks.values():
+                for lock, exits in screen_lock.items():
+                    for duo in exits:
+                        for exit in duo:
+                            if exit in net.shortest_path(graphik, spawn):
+                                accessible_locks.add((lock, exit))
+            return list(accessible_locks)
+
+        def use_lock(graphik, locks, lock, used):  # Returns the new graphic after using the item specified on the lock specified.!
+            copy_g = deepcopy(graphik)
+
+            used_item = lock[0]
+            which_spawn = lock[1]
+            which_screen = self.B7_screen(int(which_spawn[:-3]))
+            for lock, links in locks[which_screen].items():
+                if lock != used_item:
+                    continue
+                for edge in links:
+                    copy_g.add_edge(*edge)
+                copy_g.remove_node(used)
+            del locks[self.B7_screen(int(which_spawn[:-3]))][used_item]
+
+            if self.world_i == 0:
+                if which_screen == 5:
+                    del locks[8][used_item]
+                elif which_screen == 8:
+                    del locks[5][used_item]
+            if self.world_i == 1:
+                if which_screen == 26:
+                    del locks[28][used_item]
+                elif which_screen == 28:
+                    del locks[26][used_item]
+
+
+            return copy_g, locks
+
+        def play(graph, spawn, locks):
+            # Check if locks are accessible:
+            accessible_locks = get_locks(graph,locks, spawn)
+            accessible_items = get_items(graph, spawn)
+            if not accessible_locks:  # Tout est supposément débarré.
+                accessible_spawn = net.shortest_path(graph, spawn).keys()
+                for sortie in self.Exits:
+                    if str(sortie) not in accessible_spawn:
+                        net.draw(graph, with_labels=True)
+                        plt.savefig("recursive_bug.png", format="PNG")
+                        plt.clf()
+                        raise LogicError(f"{sortie} not accessible")
+                #raise BaseException("Tout est accessible!")
+            else: # Il reste encore des trucs à débarrer.
+                if not accessible_items:
+                    raise LogicError(f"Il n'y a plus d'objets disponibles.")
+                for_verif = [x[1] for x in accessible_items]
+                if not any([x[0] in for_verif for x in accessible_locks]):
+                    net.draw(graph, with_labels=True)
+                    plt.savefig("recursive_bug_noitems.png", format="PNG")
+                    plt.clf()
+                    raise LogicError("Les objets accessibles ne peuvent débloquer les locks actuels.")
+                for lock in accessible_locks:
+                    for objet in accessible_items:
+                        if lock[0] != objet[1]:  # Cet objet ne débarre pas.
+                            continue
+                        new_locks = deepcopy(locks)
+                        new_graph, after_use_lock = use_lock(graph,new_locks, lock, objet)
+                        play(new_graph, lock[1], after_use_lock)
+            return True
+
+        g = self.nodes()
+        initial_locks = deepcopy(self.Doors.world_locks)
+
+        print("Starting the recursion...")
+        play(g, "0 (N)", initial_locks)
+        return True
+
+
+    def B7_screen(self, B7):
+        return self.screens[B7]
 
 
     def nodes(self):
+        # Add something for internal puzzles. Like in the cave.
+
+
         g = net.compose(self.Exits.nodes(), self.Items.nodes(self.Exits))
+        net.draw(g, with_labels=True)
+        plt.savefig("recursive_world1bug.png", format="PNG")
+        plt.clf()
+        for x in self.Doors.nodes(self.Exits, self.Items).edges():
+            g.remove_edge(x[0], x[1])
         return g
 
 
